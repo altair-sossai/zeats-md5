4309324984c42c298768e93b478cfc7ffd416291
Altair Sossai <altairsossai@gmail.com>
2021-02-05 17:10:40 -0300

    Inicio do projeto

---
+++ Zeats.MD5/Extensions/ByteExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Zeats.MD5.Extensions
+{
+    public static class ByteExtensions
+    {
+        public static byte[] ComputeHash(this byte[] bytes)
+        {
+            using var md5 = System.Security.Cryptography.MD5.Create();
+
+            return md5.ComputeHash(bytes);
+        }
+    }
+}
---
+++ Zeats.MD5/Extensions/StreamExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace Zeats.MD5.Extensions
+{
+    public static class StreamExtensions
+    {
+        public static byte[] ComputeHash(this Stream stream)
+        {
+            using var md5 = System.Security.Cryptography.MD5.Create();
+
+            return md5.ComputeHash(stream);
+        }
+    }
+}
